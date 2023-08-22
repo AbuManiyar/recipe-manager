f23fe1eccd9fa8dfdbe3071850c61e0f37c787c4
Abutalha <maniyarabutalha00@gmail.com>
2023-08-22 23:28:03 +0530

    new user added

--- app.py
+++ app.py
@@ -11,12 +11,13 @@ def index():
 
 @app.route("/view", methods=['POST'])
 def view():
+    cursor.execute('create database if not exists recipe_manager')
     name = request.form['Name']
     cursor.execute('use recipe_manager')
-    cursor.execute('create table if not exists iamuser(id int);')
     cursor.execute('show tables;')
     users = cursor.fetchall()
     list_user=[]
+    print(users)
     for i in range(len(users[0])):
         list_user.append(users[0][i])
     if name in list_user: 
@@ -32,5 +33,16 @@ def view():
     return render_template('view.html', obtained_name= name)
 
 
+@app.route('/add', methods=['POST'])
+def add():
+    name = request.form['username']
+    email = request.form['useremail']
+    cursor.execute(f'create table if not exists {name}(email varchar(40), primary key (id));')
+    cursor.execute(f"insert into {name} value('{email}')")
+    return 'Successful'
+
+@app.route('/new')
+def new():
+   return render_template('newuser.html') 
 if __name__ == '__main__':
     app.run(debug=True)
